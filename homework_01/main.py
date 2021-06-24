24600c451faa89b061a242bcdd33292a97d05d6c
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2021-06-25 00:11:15 +0600

    i am sorry, dont kill me

--- homework_01/main.py
+++ homework_01/main.py
@@ -39,33 +39,6 @@ def filter_numbers(*some_list):
         return list(filter(is_prime, some_list[0]))
 
 
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
-odd_list = []
-even_list = []
 
-
-def is_prime(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-    if n == d:
-        return True
-
-
-def filter_numbers(*some_list):
-    if some_list[-1] == 'odd':
-        for i in some_list[0]:
-            if i % 2 != 0:
-                odd_list.append(i)
-        return odd_list
-    elif some_list[-1] == 'even':
-        for i in some_list[0]:
-            if i % 2 == 0:
-                even_list.append(i)
-        return even_list
-    elif some_list[-1] == 'prime':
-        return list(filter(is_prime, some_list[0]))
 
 
