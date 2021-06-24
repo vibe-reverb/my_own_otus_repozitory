c3f071a7dd5062f7fe436f92b76948678994150b
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2021-06-25 00:01:29 +0600

    new rem

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,49 +1,12 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-list_of_num = []
 
 
 def power_numbers(*n):
+    list_of_num = []
     for number in n:
         list_of_num.append(number ** 2)
     print(list_of_num)
-    list_of_num.clear()
+    return list_of_num
 
-
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
-odd_list = []
-even_list = []
-
-
-def is_prime(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-    if n == d:
-        return True
-
-
-def filter_numbers(*some_list):
-    if some_list[-1] == 'odd':
-        for i in some_list[0]:
-            if i % 2 != 0:
-                odd_list.append(i)
-        return odd_list
-    elif some_list[-1] == 'even':
-        for i in some_list[0]:
-            if i % 2 == 0:
-                even_list.append(i)
-        return even_list
-    elif some_list[-1] == 'prime':
-        return list(filter(is_prime, some_list[0]))
 
 
 ODD = "odd"
@@ -76,6 +39,33 @@ def filter_numbers(*some_list):
         return list(filter(is_prime, some_list[0]))
 
 
-power_numbers(1, 2, 3)
-power_numbers(1, 2, 3)
-print(power_numbers(1 ,2 ,3))
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+odd_list = []
+even_list = []
+
+
+def is_prime(n):
+    d = 2
+    while n % d != 0:
+        d += 1
+    if n == d:
+        return True
+
+
+def filter_numbers(*some_list):
+    if some_list[-1] == 'odd':
+        for i in some_list[0]:
+            if i % 2 != 0:
+                odd_list.append(i)
+        return odd_list
+    elif some_list[-1] == 'even':
+        for i in some_list[0]:
+            if i % 2 == 0:
+                even_list.append(i)
+        return even_list
+    elif some_list[-1] == 'prime':
+        return list(filter(is_prime, some_list[0]))
+
+
